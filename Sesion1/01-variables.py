569b39a1c9afb4ea5bd017055abfef8c1774cfe4
Javier Barreda Joo <javierbarredajoo@hotmail.com>
2021-12-28 00:44:02 -0500

    segundo gran commit

---
+++ Sesion1/01-variables.py
@@ -0,0 +1,25 @@
+number=10
+# variable numerica
+numero2=10.5
+#variables de texto o string:
+nombre="Javier"
+apellido='Barreda'
+html='''<html>
+<p>
+</p>'''
+
+print('Buenas noches Perú. Gracias por todo')
+print(type(nombre))
+print(type(number))
+print(type(numero2))
+
+edades = [14,34,32,34,78,35,13,52,31,42,74,'alfredo', True, [14,54]]
+
+#En javascript tenemos los json que son diccionarios. Si no se una palabra busco en el diccinario
+# y ahi saco su valor. Pues lo mismo en python. json=javascrip Object Notation
+
+curso = {
+    'nombre':'Backend',
+    'nombre2':'Frontend',
+}
+print(curso['nombre2'])
