38a10284798189929c63fbfee06adcdbd87b66cf
Javier Barreda Joo <javierbarredajoo@hotmail.com>
2021-12-28 23:37:40 -0500

    nuevo commit 3

--- Sesion2/04-colecciones-datos.py
+++ Sesion2/04-colecciones-datos.py
@@ -21,3 +21,49 @@ print(id(lista3))
 print('La lista 1 es', lista1)
 print('La lista 2 es', lista2)
 print('La lista 3 es', lista3)
+
+#Tuplas , ahora veremos que hay en ellas
+alumnos=('Javier','Humberto','Lorena','Maria Paz')
+
+#Ejemplo:
+
+CONFIGURACION=(
+    {
+        'Nombre':'API_KEY',
+        'Valor':'XXX.XXXXX.XXX'
+    },
+    {
+        'Nombre':'Sendgrid',
+        'Valor':'Infinito'
+    })
+#Si una tupla tiene internamente una estructura de datos cambiable, si se podra cambiar.
+#En cambio si hay dato primitivo como STR, INF, etc Ahi si no se puede.
+
+CONFIGURACION[0]['Nombre']='XD'
+CONFIGURACION=('76876234234','781273912312','9875677623')
+
+
+print(CONFIGURACION)
+print(CONFIGURACION[1])
+#DICCIONARIOS:
+#Coleccion de elementos que estan inexados y cuentan con una llave o clave y su valor de contenido
+# se puedne modificar el contenido y ademas s epuede crear nuevas llaves
+
+persona= {
+    'nombre':'Javier',
+    'correo':'javier.barreda@utec.edu.pe',
+    'inversiones':[
+        {
+            'fuente':'binance',
+            'tipo':'bitcoin'
+        },
+        {
+            'fuente':'netmask',
+            'tipo':'NFT'
+        }
+    ],
+}
+#podemos agregar una nueva seccion al diccionario:
+persona['estatura']='1.72'
+print(persona.keys())
+print(persona.values())
