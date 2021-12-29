5386b4391c182466acdd4e199768e007f95389d1
Javier Barreda Joo <javierbarredajoo@hotmail.com>
2021-12-28 20:46:57 -0500

    sesion 2

---
+++ Sesion2/03-operadores.py
@@ -0,0 +1,19 @@
+# Operadores de asignacion
+# Igual=
+# Incremento +=
+# Decremento -=
+# Multiplicación *=
+# Division /=
+# Potencia **=
+numero = 100
+
+numero +=5
+print(numero)
+
+#Aqui tenemos los famosos operadores de asignacion
+
+#Operadores de comparación
+# == para saber si tienen el mismo valor
+# != para saber si son diferentes
+# <,> para saber si es mayor o menor
+# >= ,<=, para saber si es mayor igual o menor igual
