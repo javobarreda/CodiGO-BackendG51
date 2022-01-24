de1cda3a053c5305c45be3bc5452e706923872e9
Javier Barreda Joo <javierbarredajoo@hotmail.com>
2022-01-24 00:25:43 -0500

    chore:ejercicio herencia

--- Sesion01/3-clase-con-herencia.py
+++ Sesion01/3-clase-con-herencia.py
@@ -18,17 +18,24 @@ class Vehiculo(object):
         self.__velocidad -= 20
         return self.__velocidad
 
+    def get_velocidad(self):
+        return self.__velocidad
+
 
 class VehiculoVolador(Vehiculo):
     def __init__(self, color, modelo, traccion, vuela=False):
         super().__init__(color, modelo, traccion)
-        self.vuela = vuela
+        self.__vuela = vuela
 
     def volar(self):
-        self.vuela = True
+        self.__vuela = True
 
     def aterrizar(self):
-        self.vuela = False
+        self.__vuela = False
+
+    def estado(self):
+        estado_volando = 'esta volando' if self.__vuela else 'esta aterrizando'
+        return 'el vehiculo es de color: {} modelo: {} traccion: {} velocidad: {} y {}'.format(self.color, self.modelo, self.traccion, self.get_velocidad(), estado_volando)
 
 
 class VehiculoOffRoad(VehiculoVolador):
@@ -39,7 +46,7 @@ class VehiculoOffRoad(VehiculoVolador):
 obj_vehiculo = Vehiculo('verde', 'rx5', '4x4')
 obj_vehiculo_volador = VehiculoVolador('blanco', 'xyz', '4x2')
 obj_vehiculo_volador.volar()
-print(obj_vehiculo_volador.vuela)
+# print(obj_vehiculo_volador.vuela)
 print(obj_vehiculo_volador.color)
 
 # print(obj_vehiculo.vuela)
@@ -48,3 +55,6 @@ obj_vehiculo_offroad = VehiculoOffRoad('azul', 'asda', '4x2')
 obj_vehiculo_offroad.acelerar()
 obj_vehiculo_offroad.desacelerar()
 obj_vehiculo_offroad.volar()
+
+# Primeor que el atributo vuela sea privado y luego tener un metodo llamado estado en el cual me indique cual es el estado del vehiculo, que me diga su color, modelo, traccion, velocidad y si esta volando o si est aterrizando
+print(obj_vehiculo_volador.estado())
