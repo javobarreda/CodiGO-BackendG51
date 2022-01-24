1deee57a4e84e83d1720a7272f149da1581d37c5
Javier Barreda Joo <javierbarredajoo@hotmail.com>
2022-01-23 19:59:47 -0500

    antes de pasarsemana03

---
+++ Sesion02/tarea.py
@@ -0,0 +1,4 @@
+#hacer una api en la cual se almacenen todos los usuarios de una compañoa, se debe guardar el nombre, apellido, area y fecha-inicio, DNI
+#Vamos a tener un registro y un despido para eliminar el usuario (usar el metodo DELETE) ademas se puede actualizar el usuario con uno de sus campos(si yo quiero solamente puedo actualziar o el nombre o el apellido, etc)
+#Ademas tener un endpoint para devolver el usuario PERO segun su DNI http://127.0.0.1:5000/usuario/12345678
+#Implementar un front para este backend
